b3fb3128d4eb6737bac83672c8f440eeaa0a67ef
yasser yahia <yasser.yahia@brightskiesinc.com>
2022-09-10 22:15:45 +0200

    before modules

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.92.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "Batman"
+  address_space       = ["10.0.0.0/16"]
+  location            = "East US"
+  resource_group_name = "vnet_resource_group"
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "Robin"
+  resource_group_name  = "vnet_resource_group"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "subnet2" {
+  name                 = "Robin2"
+  resource_group_name  = "vnet_resource_group"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
