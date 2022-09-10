7fb5478852aa7e45a53d308114cb0dc32d4c3035
yasser yahia <yasser.yahia@brightskiesinc.com>
2022-09-10 22:31:16 +0200

    working on vnet-module

---
+++ modules/vnet-module/variables.tf
@@ -0,0 +1,15 @@
+variable "vnet_name" {
+	type = string
+}
+
+variable "vnet_rg_name" {
+	type = string
+}
+
+variable "vnet_address_space" {
+	type = list
+}
+
+variable "vnet_location" {
+	type = string
+}
